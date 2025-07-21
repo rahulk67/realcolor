90930922478ff70d003e3dc53e225d3271f99f59
rahulk67 <rahulkumawat6767@gmail.com>
2025-07-21 22:22:11 +0530

    sdf and ui

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -12,22 +12,22 @@ const mysql = require('mysql2/promise');
 // });
 
 
-// const connection = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',  // XAMPP's default root password is blank
-//     database: 'goagamesclub',
-//     port: 3306
-// });
-
-
 const connection = mysql.createPool({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'Lokesh@123',
-        database: 'gamedb',
-        port: 3306
-    });
+    host: 'localhost',
+    user: 'root',
+    password: '',  // XAMPP's default root password is blank
+    database: 'goagamesclub',
+    port: 3306
+});
+
+
+// const connection = mysql.createPool({
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: 'Lokesh@123',
+//         database: 'gamedb',
+//         port: 3306
+//     });
     
 
 module.exports = connection;
